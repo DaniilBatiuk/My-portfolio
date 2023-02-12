ec849f83d48e0c875982786157038df0498ccd83
DaniilBatiuk <sportak50@gmail.com>
2023-02-12 16:50:21 +0200

    Weather_1
    WPF application with MVVM implementation using API.

---
+++ Weather/Weather/Models/Player.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Weather.ViewModels;
+
+namespace Weather.Models
+{
+    public class Player : ObservableObject
+    {
+        private string login = default!;
+        private string password = default!;
+        private string notedCity = "Kyiv";
+        public string Login
+        {
+            get => login;
+            set => SetValue(ref login, value);
+        }
+        public string Password
+        {
+            get => password;
+            set => SetValue(ref password, value);
+        }
+        public string NotedCity
+        {
+            get => notedCity;
+            set => SetValue(ref notedCity, value);
+        }
+
+    }
+}
